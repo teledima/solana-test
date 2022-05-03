95162365d5af33006ae373478b21caf61b838525
teledima <teledima00@gmail.com>
2022-05-03 20:48:01 +0500

    Remove test app

--- app-node/lib/AnchorProvider.ts
+++
@@ -1,8 +0,0 @@
-import * as anchor from '@project-serum/anchor';
-import AnchorWallet from './AnchorWallet';
-import { Connection } from '@solana/web3.js';
-
-const network = 'http://localhost:8899';
-
-const provider = new anchor.AnchorProvider(new Connection(network), AnchorWallet.local(), { skipPreflight: false })
-export default provider;
--- app-node/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-
-export const donationAccount = new PublicKey('BvVWZLXkr3U4M2ixsXrEc6DHGAKtWpFCVZVafa3oUV3q');
-export const programId = new PublicKey('3jRGs6wCxECxgii1bWPT8E955mH29e7tksCrbGQZHAc2');
-export const ownerAccount = new PublicKey("C9sQMMkY54ZR9oTbcKbnEGwJNiNb3Zk5KR2nJzqhNuCZ");
